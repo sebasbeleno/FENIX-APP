baf3b0372ac194558ca49a7dff969e60df9f79a0
Sebastian Beleño <sebasbeleno15@gmail.com>
2023-05-27 13:48:39 -0500

    implementation

--- src/pages/monitoria/scan.tsx
+++ src/pages/monitoria/scan.tsx
@@ -1,28 +1,49 @@
-import { useRouter } from "next/router";
 import Layout from "@/components/Layout";
 import { QrReader } from "react-qr-reader";
 import { useState } from "react";
 
 export default function Page() {
-  const router = useRouter();
   const [data, setData] = useState("No result");
 
+  const renderConfirmation = () => {
+    if (data !== "No result") {
+      return (
+        <QrReader
+          onResult={(result, error) => {
+            if (!!result) {
+              setData(result?.getText());
+            }
+
+            if (!!error) {
+              console.info(error);
+            }
+          }}
+          className="w-full h-full"
+          constraints={{ facingMode: "environment" }}
+        />
+      );
+    } else {
+      return (
+        <div className="flex flex-col justify-center items-center">
+          <h1 className="text-2xl">Confirmar asistencia?</h1>
+          <div className="flex flex-row justify-center items-center">
+            <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+              Si
+            </button>
+            <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+              No
+            </button>
+          </div>
+        </div>
+      );
+    }
+  };
   return (
     <Layout>
-      <QrReader
-        onResult={(result, error) => {
-          if (!!result) {
-            setData(result?.getText());
-          }
-
-          if (!!error) {
-            console.info(error);
-          }
-        }}
-        className="w-full h-full"
-        constraints={{ facingMode: "environment" }}
-      />
-      <p>{data}</p>
+      <div className="flex flex-col justify-center items-center">
+        <h1 className="text-2xl">Escanea el código QR</h1>
+      </div>
+      {renderConfirmation()}
     </Layout>
   );
 }
