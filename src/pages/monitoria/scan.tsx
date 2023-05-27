38e9e58252de1fa1b2daf9ba62d941ab6b29af37
Sebastian Beleño <sebasbeleno15@gmail.com>
2023-05-27 13:49:51 -0500

    implementation2

--- src/pages/monitoria/scan.tsx
+++ src/pages/monitoria/scan.tsx
@@ -3,47 +3,70 @@ import { QrReader } from "react-qr-reader";
 import { useState } from "react";
 
 export default function Page() {
+  const [step, setstep] = useState(1);
   const [data, setData] = useState("No result");
 
-  const renderConfirmation = () => {
-    if (data !== "No result") {
-      return (
-        <QrReader
-          onResult={(result, error) => {
-            if (!!result) {
-              setData(result?.getText());
-            }
+  const onConfirm = () => {
+    setstep(3);
+  };
 
-            if (!!error) {
-              console.info(error);
-            }
-          }}
-          className="w-full h-full"
-          constraints={{ facingMode: "environment" }}
-        />
-      );
-    } else {
-      return (
-        <div className="flex flex-col justify-center items-center">
-          <h1 className="text-2xl">Confirmar asistencia?</h1>
-          <div className="flex flex-row justify-center items-center">
-            <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
-              Si
-            </button>
-            <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
-              No
-            </button>
+
+  const renderStepts = () => {
+    switch (step) {
+      case 1:
+        return (
+          <div className="flex flex-col justify-center items-center">
+            <h1 className="text-2xl">Escanea el código QR</h1>
+            <QrReader
+              onResult={(result, error) => {
+                if (!!result) {
+                  setData(result?.getText());
+                  setstep(2);
+                }
+
+                if (!!error) {
+                  console.info(error);
+                }
+              }}
+              className="w-full h-full"
+              constraints={{ facingMode: "environment" }}
+            />
           </div>
-        </div>
-      );
+        );
+      case 2:
+        return (
+          <div className="flex flex-col justify-center items-center">
+            <h1 className="text-2xl">Confirmar asistencia? clase ${data}</h1>
+            <div className="flex flex-row justify-center items-center">
+              <button
+                onClick={onConfirm}
+                className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+              >
+                Si
+              </button>
+              <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+                No
+              </button>
+            </div>
+          </div>
+        );
+      case 3:
+        return (
+          <div className="flex flex-col justify-center items-center">
+            <h1 className="text-2xl">Asistencia confirmada</h1>
+          </div>
+        );
+      default:
+        break;
     }
   };
-  return (
+  }
+
+    return (
     <Layout>
       <div className="flex flex-col justify-center items-center">
-        <h1 className="text-2xl">Escanea el código QR</h1>
+        {renderStepts()}
       </div>
-      {renderConfirmation()}
     </Layout>
   );
 }
